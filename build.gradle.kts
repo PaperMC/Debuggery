e5c761bec4e43993eb4e6202fa0c84232986308e
Owen <23108066+Owen1212055@users.noreply.github.com>
2022-12-02 22:29:27 -0500

    Update version + authors + api version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 allprojects {
     apply(plugin = "java")
     group = "io.papermc.debuggery"
-    version = "1.4.0-SNAPSHOT"
+    version = "1.5.0-SNAPSHOT"
 
     java {
         toolchain.languageVersion.set(JavaLanguageVersion.of(17))
