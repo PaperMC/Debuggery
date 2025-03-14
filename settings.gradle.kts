fdb71553a1358ce7f3cf7aab886c4bd76d351837
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2025-03-14 20:09:42 +0100

    Add event class tab completes to event command

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 dependencyResolutionManagement {
     repositories {
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://oss.sonatype.org/content/groups/public/")
         mavenCentral()
     }
